f9ab206c62f2830f8e10614ae180ba756f6b8c5b
yonglin222 <dydfls0429@gmail.com>
2025-06-24 15:30:57 +0900

    0624화1530

---
+++ src/a5_class/ConstructorThis.java
@@ -0,0 +1,51 @@
+package a5_class;
+
+public class ConstructorThis {
+    public static void main(String[] args) {
+        AAA a = new AAA(3,5);
+        a.print();
+        // 클래스의 필드와 메서드의 지역변수가 같으면 필드를 지역변수로 인식하므로
+        // 명시적으로 this를 사용하여 필드를 표시해야 함. (this.필드)
+        a.print(); // 0, 5
+        BBB b1 = new BBB();
+        b1.print(); // 1 2 3 4
+        BBB b2 = new BBB(10);
+        b2.print(); // 10 2 3 4 (m1만 10으로 초기화)
+        BBB b3 = new BBB(20, 30);
+        b3.print(); // 20 30 3 4
+    }
+}
+class AAA {
+    int m;
+    int n;
+    AAA(int m, int n) {
+        m = m; //   대입연산자 왼쪽 m dms 필드의 m이 아니라 생성자의 매개변수 m을 의미함
+        this.n = n;
+    }
+    void print() {
+        System.out.println(m + " " + n);
+    }
+}
+class BBB {
+    int m1;
+    int m2;
+    int m3;
+    int m4;
+    BBB() { // 기본생성자
+        this.m1 = 1;
+        this.m2 = 2;
+        this.m3 = 3;
+        this.m4 = 4;
+    }
+    BBB(int m1) {
+        this(); // this 메서드는 자신이 속한 클래스내부의 다른 생성자를 호출하는것
+        this.m1 = m1;
+    }
+    BBB(int m1, int m2) {
+        this(m1);
+        this.m2 = m2;
+    }
+    void print() {
+        System.out.println(m1 + " " + m2 + " " + m3 + " " + m4);
+    }
+}
---
+++ src/a6_array/Array1.java
@@ -0,0 +1,49 @@
+package a6_array;
+
+//같은 패키지에 있는 클래스가 아니므로 import 해야함
+// public class라 하더라도 다른패키지에 있으면 import 해야만 사용가능
+// public의 의미는 누구나 import 할 수 있다는 의미
+// 어디있는지 자동으로 찾는다는 의미는 아님
+import a5_class.Student;
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        // 배열 Array
+        // 자료구조 (Data Structure)
+        // 자료구조의 종류 : 배여, 리스트, 세트, 맵, 트리...
+
+        // 배열의 선언 // 자바배열 수정불가
+
+        // 한줄로 선언
+        int[] array1 = new int [3]; // int형 자료 3개를 힙 영역에 생성
+        // 두줄로 선언
+        double[] array2;
+        array2 = new double[5]; // double형 자료 5개를 힙 영역에 생성
+        // 배열의 특징 : 생성시에 크리를 셜정, 늘리거나 줄이기 X
+        // 만약 array2의 크리를 6개로 늘리려면 새로 6개를 만들고 기존배열은 삭제함
+
+        // 모든 자료형을 배열로 만들 수 있음(참조자료형 포함)
+        String[] array3 = new String[16];
+        Student[] students = new Student[100]; // Student 객체 100개를 생성
+
+        // 배열에 값을 대입하는 법
+        array1[0] = 0; // 0번 인덱스(=0번 자리) index
+        array1[1] = 10; // 1번 인덱스
+        array1[2] = 20; // 2번 인덱스
+        // array1[3] = 30; // 에러. 인덱스 초과(array1은 생성시 3개로 만듬)
+
+        // 화면출력
+        System.out.println(array1[0] + " " + array1[1] + " " + array1[2]);
+        System.out.println(Arrays.toString(array1));
+        // 배열의 출력 메서드(toString)을 사용하면 쉽게 전테 데이터를 확인 가능
+
+        // 배열의 초기값 정리
+        // 배열은 힙메모리에 생성되므로 기본자료형의 경우, 각자의 초기값을 갖는다
+        // char ch = null
+        // boolean bool = false
+        // int i = 0 (byte, short, int, long 모두 0)
+        // double num = 0.0 (float, double 모두 0.0)
+    }
+}
