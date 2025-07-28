04862c0a2d9e9887d1f8006611c4d519a0a79982
yonglin222 <dydfls0429@gmail.com>
2025-07-29 08:51:48 +0900

    1234

--- src/jdbc/Customer.java
+++ src/jdbc/Customer.java
@@ -2,12 +2,114 @@ package jdbc;
 
 public class Customer {
     private String customerId; // 고객번호
-    private String companyName;// 고객회사명
+    private String companyName; // 고객회사명
     private String contactName; // 담당자명
-    private String contactTitle; // 담담다 직위
+    private String contactTitle; // 담당자직위
     private String address; // 주소
     private String city; // 도시
     private String region; // 지역
     private String phone; // 전화번호
     private int mileage; // 마일리지
+
+    public Customer() {
+    }
+
+    public Customer(String customerId, String companyName, String contactName, String contactTitle, String address, String city, String region, String phone, int mileage) {
+        this.customerId = customerId;
+        this.companyName = companyName;
+        this.contactName = contactName;
+        this.contactTitle = contactTitle;
+        this.address = address;
+        this.city = city;
+        this.region = region;
+        this.phone = phone;
+        this.mileage = mileage;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactTitle() {
+        return contactTitle;
+    }
+
+    public void setContactTitle(String contactTitle) {
+        this.contactTitle = contactTitle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", contactName='" + contactName + '\'' +
+                ", contactTitle='" + contactTitle + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", region='" + region + '\'' +
+                ", phone='" + phone + '\'' +
+                ", mileage=" + mileage +
+                '}';
+    }
 }
---
+++ src/jdbc/JdbcExample1.java
@@ -0,0 +1,53 @@
+package jdbc;
+
+import a8_modifier.Modifier1.pack2.C;
+
+import java.sql.*;
+
+public class JdbcExample1 {
+    // #1. 데이터베이스 연결 정보
+    private static final String URL = "jdbc:mariadb://localhost:3306/testdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    // #2. 정보를 조회하는 메서드 정의
+    // 고객번호로 고객 정보 조회
+    public Customer getCustomer(String customerId){
+        Customer customer = null;
+        String query = "select * from 고객 where 고객번호 = ?";
+        try (
+            Connection connection = DriverManager.getConnection(
+                    URL, USER, PASSWORD);
+            PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, customerId);
+            try (ResultSet resultSet = ps.executeQuery()){
+                System.out.println("연결성공");
+                // O-R 매핑 (Object - Relation)
+                if (resultSet.next()) {
+                    customer = new Customer();
+                    customer.setCustomerId(resultSet.getString("고객번호"));
+                    customer.setCompanyName(resultSet.getString("고객회사명"));
+                    customer.setContactName(resultSet.getString("담당자명"));
+                    customer.setContactTitle(resultSet.getString("담당자직위"));
+                    customer.setAddress(resultSet.getString("주소"));
+                    customer.setCity(resultSet.getString("도시"));
+                    customer.setRegion(resultSet.getString("지역"));
+                    customer.setPhone(resultSet.getString("전화번호"));
+                    customer.setMileage(resultSet.getInt("마일리지"));
+                }
+            }
+        }catch (SQLException e) {
+            System.out.println("에러 발생");
+        }
+        return customer;
+    }
+
+    public static void main(String[] args) {
+        // 데이터베이스 연결
+        // sql 쿼리를 전송해서 응답을 받음
+        // 응답받은 정보를 클래스에 매핑
+        JdbcExample1 repository = new JdbcExample1();
+        Customer foundCustomer = repository.getCustomer("AIHTR");
+        System.out.println(foundCustomer);
+    }
+}
