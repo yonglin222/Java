cb9281c6b8f9c53c80aaa7eed2f085709bdc6753
yonglin222 <dydfls0429@gmail.com>
2025-07-28 15:34:47 +0900

    000

---
+++ src/jdbc/Customer.java
@@ -0,0 +1,13 @@
+package jdbc;
+
+public class Customer {
+    private String customerId; // 고객번호
+    private String companyName;// 고객회사명
+    private String contactName; // 담당자명
+    private String contactTitle; // 담담다 직위
+    private String address; // 주소
+    private String city; // 도시
+    private String region; // 지역
+    private String phone; // 전화번호
+    private int mileage; // 마일리지
+}
